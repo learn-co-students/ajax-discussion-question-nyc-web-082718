6433d33d026526043cb26ea32fd6055853adff56
sinyoungnam <sinnam90@gmail.com>
2018-10-16 09:20:08 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
 });
+
+// 1. ('Hello'), ('Sup?'), ('Hi')
+// 2. undefined
+// 3. Object
+// 4. Object
+// 5. Executes in random order
+// 6. Traces a user's request flow 
